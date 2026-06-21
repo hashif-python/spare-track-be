8aa739875ad3aa1c3a366997264ec29249a5c897
Hashif <hashifmuhammed28@gmail.com>
2026-06-21 22:56:05 +0530

    updated main.py

--- app/main.py
+++ app/main.py
@@ -26,7 +26,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://spare-track-fe.vercel.app",
+        "https://spare-track-fe-qgnt.vercel.app",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
